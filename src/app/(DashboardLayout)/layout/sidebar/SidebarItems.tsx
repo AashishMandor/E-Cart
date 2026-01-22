ba2ef4301f4c30d22fb3c7a5d3b8bdc195d2525a
AashishMandor <159603683+AashishMandor@users.noreply.github.com>
2026-01-22 13:09:37 +0530

    fix: Menu children type error

--- src/app/(DashboardLayout)/layout/sidebar/SidebarItems.tsx
+++ src/app/(DashboardLayout)/layout/sidebar/SidebarItems.tsx
@@ -27,7 +27,9 @@ const renderMenuItems = (items: any, pathDirect: any) => {
         <Menu
           subHeading={item.subheader}
           key={item.subheader}
-        />
+        >
+        <></>
+        </Menu>
       );
     }
 
